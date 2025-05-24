893d21d01b86a03561f424871628a173540ffa6f
Putri Arini R <paja2367@gmail.com>
2025-05-25 00:15:51 +0700

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-expand-lg shadow-sm sticky-top" style="background-color:rgb(111, 143, 198);">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Arahphone</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link active" href="indexTA.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">About us</a></li>
+                <li class="nav-item"><a class="nav-link active" href="logoutTA.php">Logout</a></li>
+                <li class="nav-item"><a class="nav-link" href="pemesanan.php">Pemesanan</a></li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#merk" role="button" data-bs-toggle="dropdown" aria-expanded="false">Merk</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="samsung.php">Samsung</a></li>
+                        <li><a class="dropdown-item" href="xiaomi.php">Xiaomi</a></li>
+                        <li><a class="dropdown-item" href="realme.php">Realme</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
